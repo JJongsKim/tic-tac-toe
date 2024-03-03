14a68573a4635155ff5877e5cf0d79715beb382e
JJongsKim <totnwjd530@gmail.com>
2024-03-03 10:41:13 +0900

    :construction: Setting: eslint 수정 및 필요한 목업데이터 추가

---
+++ src/utils/mock.ts
@@ -0,0 +1,62 @@
+export const boardSize = [
+  { type: 1, condition: '3X3' },
+  { type: 2, condition: '4X4' },
+  { type: 3, condition: '5X5' },
+];
+
+export const winnerConditions = [
+  {
+    type: 1,
+    condition: '3',
+  },
+  {
+    type: 2,
+    condition: '4',
+  },
+  {
+    type: 3,
+    condition: '5',
+  },
+  {
+    type: 4,
+    condition: '6',
+  },
+];
+
+export const userMark = [
+  {
+    type: 1,
+    condition: '동그라미',
+  },
+  {
+    type: 2,
+    condition: '엑스',
+  },
+  {
+    type: 3,
+    condition: '세모',
+  },
+  {
+    type: 4,
+    condition: '네모',
+  },
+];
+
+export const userMarkColor = [
+  {
+    type: 1,
+    condition: '파랑',
+  },
+  {
+    type: 2,
+    condition: '빨강',
+  },
+  {
+    type: 3,
+    condition: '노랑',
+  },
+  {
+    type: 4,
+    condition: '초록',
+  },
+];
